b58e16365f8b173e28df4ca11225fe46f287c619
Zoey <zoey@panties.moe>
2020-08-01 22:07:27 -0400

    Initial commit.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,138 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <h2>Yuri Viewer</h2>
+      </div>
+
+      <v-spacer />
+
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+           <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+            link
+            href="https://github.com/NotZoeyDev/CuteYuriBot"
+            target="blank"
+          >
+            <v-icon>mdi-code-tags</v-icon>
+          </v-btn>
+        </template>
+        <span>Bot source code!</span>
+      </v-tooltip>
+
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+           <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+            link
+            href="https://twitter.com/CuteYuriBot"
+            target="blank"
+          >
+            <v-icon>mdi-twitter</v-icon>
+          </v-btn>
+        </template>
+        <span>The Twitter account!</span>
+      </v-tooltip>
+    </v-app-bar>
+
+    <v-main>
+      <v-container
+        class="fill-height"
+      >
+        <v-row
+          justify="center"
+        >
+          <v-col
+            lg=6
+            xs=12
+            
+          >
+            <v-card
+              elevation=4
+            >
+              <v-toolbar color="primary">
+                <v-toolbar-title class="white--text">{{ posts[post].title }}</v-toolbar-title>
+              </v-toolbar>
+              <v-carousel
+                hide-delimiters
+                @change="setId"
+                width="auto"
+              >
+                <v-carousel-item
+                  v-for="(post,i) in posts"
+                  :key="i"
+                  :src="post.img_url"
+                  contain
+                >
+                </v-carousel-item>
+              </v-carousel>
+
+              <v-card-actions>
+                <v-btn
+                  color="primary"
+                  @click="showOriginal"
+                >
+                  View original
+                </v-btn>
+
+                <v-btn
+                  v-if="posts[post].source_url != ''"
+                  color="primary"
+                  @click="showSource"
+                >
+                  View source
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import axios from 'axios';
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  data: () => ({
+    posts: [],
+    post: 0,
+    post_id: 0,
+    loaded: false
+  }),
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  async mounted() {
+    axios.get("https://yuri-backend.panties.moe/posts.json").then((response) => {
+      this.posts = response.data;
+      this.post_id = this.posts[0].id;
+    });
+  },
+
+  methods: {
+    setId(value) {
+      this.post = value;
+      
+    },
+
+    showOriginal() {
+      window.open(`https://reddit.com/${this.posts[this.post].post_url}`, '_blank');
+    },
+
+    showSource() {
+      window.open(this.posts[this.post].source_url, '_blank');
+    }
+  }
+};
+</script>
